cc90a9c098700a278c691740dea9b788ca1e80f3
Kaio Duarte <duartedossantos@gmail.com>
2019-01-26 07:20:38 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# CRA Customized
+
+Just some adjustments to the default CRA setup :)
+
+## Tweaks
+
+- Gulp
+- Storybook
+- Standard.js
+- Hot loader
+
+## Scripts
+
+- yarn start
+- yarn test
+- yarn test:watch
+- yarn lint
+- yarn lint:fix
+- yarn lint:watch
+- yarn build
+- yarn storybook
+- yarn build-storybook
+- yarn eject
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './app'
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    ReactDOM.render(<NextApp />, document.getElementById('root'))
+  })
+}
